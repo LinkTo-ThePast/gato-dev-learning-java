d1e06b62d7e79ae9ac18b345ba3cfc18ba4d6908
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-03-06 21:12:30 -0600

    chore(BoardConsole): fixing data hiding issues

--- source/src/console_version/BoardConsole.java
+++ source/src/console_version/BoardConsole.java
@@ -1,9 +1,8 @@
 package console_version;
-
 import java.util.Arrays;
 
 /**
- * Represents the actual "gato" (tic tac toe) board
+ * Represents the actual "gato" (tic-tac-toe) board
  * Theory concept applied: encapsulate board state and enforces controlled access
  * through accessor and mutators methods (studied concept from book: data hiding)
 */
@@ -39,26 +38,31 @@ public class BoardConsole {
 
     // interface 2. draw a new symbol in the board
     // this is a mutator method
-    public char[][] drawOverBoard(int selectedRow, int selectedColum, char playableChar) {
-        // initialize play session board
-        // this creates a local variable, so then this method is acting like a pure function rather than modifying current state
-        char[][] board = getBoard();
+
+    /**
+     *
+     * @param selectedRow: row index selected by player [from 1 to 3]
+     * @param selectedColum: column index selected by player [from 1 to 3]
+     * @param playableChar: character that is going to be drawn -> 'X' or 'O'
+     * @throws IllegalArgumentException if position is out bonds
+     */
+    public void drawOverBoard(int selectedRow, int selectedColum, char playableChar) {
 
         // avoid negative integers or greater than the current domain
-        boolean isNegative = selectedColum < 0 || selectedRow < 0;
+        boolean isNegative = selectedColum <= 0 || selectedRow <= 0;
         boolean isGreater = selectedColum > board.length || selectedRow > board.length;
         // zero index selection
         if (isGreater || isNegative) {
-            return board;
+            throw new IllegalArgumentException("Position selected must be between 1 and " + BOARD_SIZE +
+                    ". " + "Selected position: " + "Row -> " + selectedRow + " / Column ->  " + selectedColum);
         }
         // we assured correct user's input
         selectedRow -= 1;
         selectedColum -= 1;
         // draw over board with specified positions [x: row, y: column]
-        board[selectedRow][selectedColum] = playableChar;
 
-        // return modified board
-        return board;
+        this.board[selectedRow][selectedColum] = playableChar;
+
     }
 
 }
