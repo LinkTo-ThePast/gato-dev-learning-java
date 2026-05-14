78bcb7d7c184c275359596b2f7f8d9122cc1d4a8
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-05-13 23:29:31 -0600

    chore(board): change initial characters

--- source/src/console_version/BoardConsole.java
+++ source/src/console_version/BoardConsole.java
@@ -20,7 +20,7 @@ public class BoardConsole {
      */
     public BoardConsole() {
         this.board = new char[][]{
-                {'a', 'b', 'c'},
+                {'u', 'b', 'c'},
                 {'d', 'e', 'f'},
                 {'g', 'h', 'i'}
         };
